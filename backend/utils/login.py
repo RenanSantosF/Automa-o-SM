2cd59c12ee5c94ef3bece35dc9d53ad8a57402c0
RenanSantosF <77036943+RenanSantosF@users.noreply.github.com>
2025-05-18 16:53:24 +0000

    teste

--- backend/utils/login.py
+++ backend/utils/login.py
@@ -1,62 +1,5 @@
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
-# from selenium.common.exceptions import (
-#     NoSuchElementException,
-#     TimeoutException,
-#     WebDriverException
-# )
-# import time
-# import tempfile
-
-# def login_apisul(usuario, senha):
-#     try:
-#         options = webdriver.ChromeOptions()
-#         options.add_argument("--window-size=1920,1080")
-#         options.add_argument("--no-first-run")
-#         options.add_argument("--no-default-browser-check")
-#         options.add_argument("--disable-default-apps")
-
-#         options.add_argument("--headless")  # ou "--headless=new" para versões recentes
-#         options.add_argument('--disable-gpu')
-#         options.add_argument('--disable-software-rasterizer')
-
-
-
-#         temp_user_data_dir = tempfile.mkdtemp()
-#         options.add_argument(f"--user-data-dir={temp_user_data_dir}")
-
-#         driver = webdriver.Chrome(options=options)
-#         driver.get("https://novoapisullog.apisul.com.br/Login")
-#         time.sleep(2)
-
-#         try:
-#             campo_usuario = driver.find_element(By.ID, "txtUsuario")
-#             campo_senha = driver.find_element(By.ID, "txtSenha")
-#         except NoSuchElementException:
-#             driver.quit()
-#             raise Exception("Campo de login ou senha não encontrado na página")
-
-#         campo_usuario.send_keys(usuario)
-#         campo_senha.send_keys(senha)
-#         campo_senha.send_keys(Keys.RETURN)
-
-#         time.sleep(3)  # Espera redirecionar após login
-
-#         # Verifica se o login foi bem-sucedido
-#         if "Login" in driver.title or driver.current_url.endswith("/Login"):
-#             driver.quit()
-#             raise Exception("Falha no login: verifique o usuário e senha")
-
-#         return driver
-
-#     except WebDriverException as e:
-#         raise Exception(f"Erro ao iniciar o navegador: {str(e)}")
-#     except Exception as e:
-#         raise Exception(f"Erro durante login: {str(e)}")
-
-
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -89,7 +32,7 @@ def login_apisul(usuario, senha):
 
         driver = webdriver.Chrome(options=options)
         driver.get("https://novoapisullog.apisul.com.br/Login")
-        
+
         WebDriverWait(driver, 20).until(
             EC.presence_of_element_located((By.ID, "txtUsuario"))
         )
