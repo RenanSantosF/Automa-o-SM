83a73e0d0e6926168f48d90f9b9fa0b7203f8a85
renan ferreira dos santos <renan_ferreira.es@outlook.com>
2025-11-01 21:45:51 -0300

    ajusta select das transportadoras

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -102,7 +102,7 @@ function App() {
                 <Route
                   path="/ocorrencias"
                   element={
-                    <PrivateRoute allowedSetores={['ocorrencia', 'admin']}>
+                    <PrivateRoute>
                       <OcorrenciasPage />
                     </PrivateRoute>
                   }
