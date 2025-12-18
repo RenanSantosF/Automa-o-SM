2621885e5b3bf8597f239d0c8eeebee0e9aa105f
renan ferreira dos santos <renan_ferreira.es@outlook.com>
2025-12-18 10:27:00 -0300

    Ajusta alguns detalhes de header, painel de administrador e cria página home

--- backend/utils/gerarToken.py
+++ backend/utils/gerarToken.py
@@ -2,13 +2,16 @@ from datetime import datetime, timedelta, timezone
 from jose import jwt
 import os
 
-SECRET_KEY = os.getenv("SECRET_KEY")      # pega a chave secreta
-ALGORITHM = os.getenv("ALGORITHM")        # pega o algoritmo
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(days=365*100))  # 🔥 1 dia
+
+    expire = datetime.now(timezone.utc) + (
+        expires_delta if expires_delta else timedelta(hours=3)
+    )
+
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
-
